07c08ee29e18d9e3234eb08e0bdb57f251d55c20
DaVo82 <danvogler14@gmail.com>
2022-06-23 15:57:31 +0200

    further work on breakdown plots, prediction app  (wip)

--- BreakDowns.R
+++ BreakDowns.R
@@ -1,4 +1,4 @@
-library(iBreakDown)
+library(dplyr)
 library(DALEX)
 
 predict.function <- function(model, new_observation) {
@@ -7,42 +7,48 @@ predict.function <- function(model, new_observation) {
 
 ##############################################################################
 
-explain_gbm_ds3 <- explain(gbm_ds3,
+explain_gbm_ds3 <- DALEX::explain(gbm_ds3,
                            data = train3[,3:40],
-                           y = train3$Sepsis == "negativ",
+                           y = case_when(train3$Sepsis == "positiv" ~ 1, 
+                                         TRUE ~ 0),
                            label = "GBM")
 
-bd_gbm3 <- break_down(explain_gbm_ds3,
-           test3[21, -2],
-           keep_distributions = TRUE,
-           predict_function = predict.function) 
+bd_gbm3 <- predict_parts(explainer = explain_gbm_ds3,
+                           new_observation = test3[21, -2],
+                           keep_distribution = FALSE,
+                           predict_function = predict.function,
+                           type = "break_down_interactions")
 
 plot(bd_gbm3)
 
 #############################################################################
 
-explain_rf_ds3 <- explain(rf_ds3,
+explain_rf_ds3 <- DALEX::explain(rf_ds3,
                            data = train3_imputed[,3:40],
-                           y = train3_imputed$Sepsis == "negativ",
+                           y = case_when(train3_imputed$Sepsis == "positiv" ~ 1, 
+                                         TRUE ~ 0),
                            label = "Random Forest")
 
-bd_rf3 <- break_down(explain_rf_ds3,
-                      test3_imputed[1, -2],
-                      keep_distributions = TRUE,
-                      predict_function = predict.function) 
+bd_rf3 <- predict_parts(explainer = explain_rf_ds3,
+                        new_observation = test3_imputed[1, -2],
+                        keep_distribution = FALSE,
+                        predict_function = predict.function,
+                        type = "break_down_interactions")
 
 plot(bd_rf3)
 
 #############################################################################
 
-explain_rf_ds2 <- explain(rf_ds2,
+explain_rf_ds2 <- DALEX::explain(rf_ds2,
                           data = train2_imputed[,4:41],
-                          y = train2_imputed$Sepsis == "negativ",
+                          y = case_when(train2_imputed$Sepsis  == "positiv" ~ 1, 
+                                        TRUE ~ 0),
                           label = "Random Forest")
 
-bd_rf2 <- break_down(explain_rf_ds2,
-                     test2_imputed[1, -2],
-                     keep_distributions = TRUE,
-                     predict_function = predict.function) 
+bd_rf2 <- predict_parts(explainer = explain_rf_ds2,
+                        new_observation = test2_imputed[1, -2],
+                        keep_distribution = FALSE,
+                        predict_function = predict.function,
+                        type = "break_down_interactions")
 
 plot(bd_rf2)
---
+++ PredictionApp.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(dplyr)
+library(shinythemes)
+library(rgbif)
+library(ggplot2)
+library(caret)
+library(ranger)
+library(gbm)
+library(DALEX)
+
+ui <- fluidPage(
+  titlePanel("Sepsis-Prädiktion"),
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons('model', 'Modell wählen', 
+                  c("Random Forest", "Gradient Boosting Machine")),
+      selectInput('patient', 'Patient wählen', unique(test3$Patient_ID)),
+      actionButton('predictButton', 'Berechnen')
+    ),
+    mainPanel(
+              verbatimTextOutput('probability'),
+              plotOutput('plot'),
+      )
+    )
+  )
+
+server <- function(input, output, session){
+  observeEvent(input$predictButton, {
+    if (input$model == "Gradient Boosting Machine") {
+      explainer <- explain_gbm_ds3
+      new_dataset <- test3
+    }
+    if (input$model == "Random Forest") {
+      explainer <- explain_rf_ds3
+      new_dataset <- test3_imputed
+    }
+    patient_index <- input$patient
+    new_data <- new_dataset[patient_index, -2]
+    prediction <- predict(explainer$model, new_data, type = "prob")[,1]
+    bd <- predict_parts(explainer = explainer,
+                        new_observation = new_data,
+                        keep_distribution = FALSE,
+                        predict_function = predict.function,
+                        type = "break_down") # interaction sehr langsam
+    output$probability <- renderPrint(
+      paste("Die Wahrscheinlichkeit für eine Sepsis beträgt:", 
+            round(prediction * 100, 1), 
+            "%"))
+    output$plot <- renderPlot(plot(bd))
+    })
+}
+
+shinyApp(ui = ui, server = server)
+
+# große Patient_ID funktionieren nicht
+# Layout des Textoutputs
