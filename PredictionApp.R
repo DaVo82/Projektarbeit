546028c2273c76fb5dac2b6554bb92c1616b4759
DaVo82 <danvogler14@gmail.com>
2022-06-24 18:56:52 +0200

    revised sepsis definition, further work on prediction app

--- PredictionApp.R
+++ PredictionApp.R
@@ -1,12 +1,14 @@
 library(shiny)
-library(dplyr)
 library(shinythemes)
-library(rgbif)
-library(ggplot2)
+library(dplyr)
 library(caret)
 library(ranger)
 library(gbm)
 library(DALEX)
+
+# Voraussetzungen: trainierte Modelle und DALEX-Explainer
+#                  aufbereitete Analysedaten (Slopes, imputierte Variante für Rf)
+#                  Datensatz mit den vollständigen Beobachtungen der Analysedaten
 
 ui <- fluidPage(
   titlePanel("Sepsis-Prädiktion"),
@@ -14,12 +16,14 @@ ui <- fluidPage(
     sidebarPanel(
       radioButtons('model', 'Modell wählen:', 
                   c("Random Forest", "Gradient Boosting Machine")),
-      selectInput('patient', 'Patienten wählen:', unique(test3$Patient_ID)),
+      selectizeInput('patient', 'Patienten wählen:',
+                     options = list(maxOptions = 1500),
+                     unique(test3$Patient_ID)),
       actionButton('predictButton', 'Berechnen')
     ),
     mainPanel(
       htmlOutput('probability'), tags$head(tags$style("#probability{font-size: 20px}")),
-              plotOutput('plot')
+      plotOutput('plot')
       )
     )
   )
@@ -34,8 +38,9 @@ server <- function(input, output, session){
       explainer <- explain_rf_ds3
       new_dataset <- test3_imputed
     }
-    patient_index <- input$patient
-    new_data <- new_dataset[patient_index, -2]
+    new_data <- new_dataset %>% 
+      filter(Patient_ID == input$patient) %>% 
+      select(-Sepsis)
     withProgress(message = 'Berechne Vorhersage...', value = 0, {
       prediction <- predict(explainer$model, new_data, type = "prob")[,1]
       bd <- predict_parts(explainer = explainer,
@@ -45,8 +50,8 @@ server <- function(input, output, session){
                           type = "break_down") # interaction sehr langsam
       })
     output$probability <- renderUI(
-      paste("Die Wahrscheinlichkeit für eine Sepsis beträgt", 
-            round(prediction * 100, 1), 
+      paste("Die Wahrscheinlichkeit für eine Sepsis beträgt",
+            round(prediction * 100, 1),
             "%"))
     output$plot <- renderPlot(plot(bd))
     })
@@ -54,4 +59,16 @@ server <- function(input, output, session){
 
 shinyApp(ui = ui, server = server)
 
-# große Patient_ID funktionieren nicht
+# evtl. zweiter Plot auf Anforderung mit einem Prädiktor im Zeitverlauf
+# tableOutput('Basisdaten'),
+# 
+# 
+# new_data_all <- data_sample %>% 
+#   filter(Patient_ID == input$patient)
+# output$Basisdaten <- renderTable({
+#   new_data_all %>% 
+#     transmute(Patient_ID, Age, Gender, Unit = case_when(!is.na(Unit1) ~ "MICU",
+#                                                         !is.na(Unit1) ~ "SICU"),
+#               HospAdmTime) %>% 
+#     unique()
+# })
